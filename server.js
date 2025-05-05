ab958d42a7a25109989846488d37676b3bb8e7d2
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-05 08:11:36 +0300

    Using the middleware in specific routes

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const logger = (req,res, next) => {
     console.log(`${req.method} request to ${req.url}`);
     next();//move to the next middleware or route handler
 }
+
+const checkTime = (req,res,next) =>{
+    const hour = 20;
+
+    if (hour>=6 && hour<= 18){
+        next(); //continue to the next function
+    } else {
+        res.send("Access denied. This API is available between 6 AM - 6 PM only. ");
+    }
+};
+
 app.use(logger)
 //start a server on port 5002
 app.listen(5002, () => {
@@ -30,7 +41,7 @@ app.get("/test", (req,res) => {
     res.send("This is a test route");
 });
 
-app.get("/date", (req,res) => {
+app.get("/date",checkTime, (req,res) => {
     res.send(`Today's date is ${new Date().toDateString()}`);
 });
 
